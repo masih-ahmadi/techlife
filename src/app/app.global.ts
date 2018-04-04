998b8ce576d58c1f1df9c09f856f2c0037b8922b
masih <masih.ahmadi.786@gmail.com>
2018-04-04 14:15:56 +0430

    final

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -1,11 +1,11 @@
 export const App: any = {
   store           : 'Ajwadates',  // change this with your app name
   ContactEmail    : 'mahmadi@techlife.af', // change this with your email contact
-  url             : 'http://localhost:8080/ionic/', // change this with your URL (please use https, recommended)
+  url             : 'http://www.ajwadates.sa/', // change this with your URL (please use https, recommended)
   OneSignalAppID  : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // change this with your onesignal api key
   GCM             : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Google Project Number for Google Cloud Messaging
-  consumerKey     : 'ck_7ea66ceda242f09336d3cdcaf0755e9055b20488', // change this with your Consumer Key from WooCommerce
-  consumerSecret  : 'cs_446cd424068d0c3e002da7f0b1f5b4c830bcc039', // change this with your Consumer Secret from WooCommerce
+  consumerKey     : 'ck_a8d14908222879c9f1719db548727b54265d78a2', // change this with your Consumer Key from WooCommerce
+  consumerSecret  : 'cs_ce2da9d7a4d3b47d93ed2d18ff0b3828cde1ff42', // change this with your Consumer Secret from WooCommerce
 
   paypalSandboxClientID: 'AZjyISbp1zmOhZ0o_iAG3W2IGjlz2hvEC-8cGoQ7fXcMFN9afaRuW0X1B1PVSgkSuTQWOKqM9N4NTkOP',
   paypalLiveClientID: '', // get this from paypal developer dashboard
